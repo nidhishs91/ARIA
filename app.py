78d11c04205cec0c6fd05786a12861716a20590b
Nidhish Singh <116066599+nidhishs91@users.noreply.github.com>
2026-05-13 22:02:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,3 @@
-# import sentry_sdk
-
-# sentry_sdk.init(
-#     dsn="https://8cdc5231e8aef63d2fd7dd94d5f4ea71@o4511369733603328.ingest.us.sentry.io/4511369780527104"
-# )
-
-# def trigger_error():
-#     return 10 / 0  # intentional error
-
-# trigger_error()
-
-# import sentry_sdk
-
-# sentry_sdk.init(
-#     dsn="https://8cdc5231e8aef63d2fd7dd94d5f4ea71@o4511369733603328.ingest.us.sentry.io/4511369780527104",
-#     traces_sample_rate=1.0
-# )
-
-# def trigger_error():
-#     return 10 / 0  # intentional error
-
-# try:
-#     trigger_error()
-# except Exception as e:
-#     event_id = sentry_sdk.capture_exception(e)
-#     print("SENTRY_EVENT_ID:", event_id)
-#     raise
-
 import sentry_sdk
 import os
 
@@ -35,7 +7,7 @@ sentry_sdk.init(
 )
 
 def trigger_error():
-    return 10 / 0  # intentional error
+    return 10 / 2  # intentional error
 
 try:
     trigger_error()
