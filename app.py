fde9aa65d7230d500905eeadc3f573c878bada09
Nidhish Singh <116066599+nidhishs91@users.noreply.github.com>
2026-05-15 23:09:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 def trigger_error():
-    return 10 / 2  # change to 10 / 0 to simulate failure
+    return 10 / 0  # change to 10 / 0 to simulate failure
 
 
 if __name__ == "__main__":
     trigger_error()
+
+
